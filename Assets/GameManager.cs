8e58f930d4c72b92ed1845cda4a641233ac8c5d9
maksutovaynur <maksutov_aynur@mail.ru>
2021-02-01 13:38:58 +0300

    Inconsistent state

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,10 +1,13 @@
-﻿using Photon.Pun;
+﻿using System;
+using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
+    public GameObject playerPrefab;
     public override void OnLeftRoom()
     {
         SceneManager.LoadScene(0);
@@ -17,11 +20,17 @@ public class GameManager : MonoBehaviourPunCallbacks
 
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
-        Debug.Log("Player " + otherPlayer.NickName + " entered Room");
+        Debug.Log("Player " + otherPlayer.NickName + " left Room");
     }
-
+    
     public void StopGame()
     {
         PhotonNetwork.LeaveRoom();
+    }
+
+    public void Start()
+    {
+        var startPosition = new Vector3(Random.Range(-2, 2), Random.Range(-4, 4));
+        PhotonNetwork.Instantiate(playerPrefab.name, startPosition, Quaternion.identity);
     }
 }
