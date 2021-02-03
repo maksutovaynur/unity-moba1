715b523ebf537473479d7ed9eaa4a42c52456438
maksutovaynur <maksutov_aynur@mail.ru>
2021-02-03 11:32:00 +0300

    Add touchscreen support

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -1,9 +1,9 @@
 ﻿using System;
+using System.Linq;
 using Photon.Pun;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
-
+using UnityEngine.EventSystems;
 
 enum AnimationState : byte
 {
@@ -18,7 +18,7 @@ enum AnimationState : byte
 
 public class PlayerControls : MonoBehaviour, IPunObservable
 {
-    public TextMeshPro nickName; 
+    public TextMeshPro nickName;
     public TextMeshPro statusBar;
     public double inputThrottleInterval = 0.1;
     public float flyPullForce = 5.0f;
@@ -27,10 +27,12 @@ public class PlayerControls : MonoBehaviour, IPunObservable
     public float jumpPullForce = 5.0f;
     public float movementSpeed = 1.6f;
     public float movementAcceleration = 8.0f;
-    
+    public GameObject fireballPrefab;
+    public double fireballInterval = 0.5f;
+
     private sbyte lookDirectionState, moveDirectionState, moveSpeedState;
     private sbyte kbJump, kbMoveDirection, kbMoveSpeed;
-    
+
     private AnimationState animationState;
     private TMP_Text playerStatusBar;
     private Animator animator;
@@ -39,35 +41,40 @@ public class PlayerControls : MonoBehaviour, IPunObservable
     private float flyPower;
     private PhotonView photonView;
     private SpriteRenderer spriteRenderer;
-    private bool inJump, grounded, groundCollision;
+    private bool inJump, grounded, groundCollision, isFiringBalls;
     private const double STILL_GROUNDED_DELTA_TIME = 0.2;
     private const double STILL_JUMP_DELTA_TIME = 0.5;
-    private double groundCollisionEnter, groundCollisionExit, lastJumpTime;
+    private double groundCollisionEnter, groundCollisionExit, lastJumpTime, lastFireballTime;
     private int groundCollisionNumber, lastJumpGroundCollisionNumber;
     private FixedJoystick joystick;
-    
+    private Vector2 fireballPoint;
+    private EventSystem eventSystem;
+
     void Start()
     {
         photonView = GetComponent<PhotonView>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
-        
+        var collider = GetComponent<Collider2D>();
+        eventSystem = GetComponent<EventSystem>();
+
         joystick = FindObjectOfType<FixedJoystick>();
         playerStatusBar = GameObject.Find("PlayerStatusBar").GetComponent<TMP_Text>();
         rigidBody = GetComponent<Rigidbody2D>();
-        
+
         moveDirectionState = 0;
         flyPower = 1.0f;
         lookDirectionState = 0;
         nickName.SetText(photonView.Owner.NickName);
         statusBar.SetText("");
         animationState = AnimationState.Idle;
-        
+
         grounded = false;
         groundCollision = false;
+        isFiringBalls = false;
         groundCollisionNumber = 0;
 
-        if (photonView.IsMine)
+        if (HasControl())
         {
             nickName.color = Color.green;
             statusBar.color = Color.green;
@@ -77,35 +84,45 @@ public class PlayerControls : MonoBehaviour, IPunObservable
         {
             nickName.color = Color.red;
             statusBar.color = Color.red;
-            rigidBody.isKinematic = false;
+            // rigidBody.bodyType = RigidbodyType2D.Static;
+            // rigidBody.gameObject.layer = 8;
+            Destroy(rigidBody);
+            Destroy(collider);
         }
+    }
+
+    private bool HasControl()
+    {
+        return photonView.IsMine || !PhotonNetwork.IsConnected;
     }
 
     void Update()
     {
-        if (photonView.IsMine || !PhotonNetwork.IsConnected)
+        if (HasControl())
         {
             GetInputs();
             FixMovement();
+            FireBalls();
             FixAnimationState();
             LogStatusBar();
         }
+
         spriteRenderer.flipX = (lookDirectionState < 0);
         animator.SetInteger("state", (int) animationState);
     }
 
     private void FixAnimationState()
-    {
-        if (grounded)
+    {            
+        if (inJump)
         {
-            if (moveDirectionState != 0) animationState = AnimationState.Walking;
-            else animationState = AnimationState.Idle;
+            animationState = AnimationState.Jumping;
         }
         else
         {
-            if (inJump)
+            if (moveDirectionState != 0) animationState = AnimationState.Walking;
+            else if (grounded)
             {
-                animationState = AnimationState.Jumping;
+                animationState = AnimationState.Idle;
             }
             else
             {
@@ -115,16 +132,16 @@ public class PlayerControls : MonoBehaviour, IPunObservable
     }
 
     private void FixMovement()
-    {        
+    {
         var currentTime = PhotonNetwork.Time;
-        
+
         bool recentColl = IsWithin(currentTime - groundCollisionEnter, 0, STILL_GROUNDED_DELTA_TIME);
         bool recentJump = IsWithin(currentTime - lastJumpTime, 0, STILL_JUMP_DELTA_TIME);
         bool sameCollision = (lastJumpGroundCollisionNumber == groundCollisionNumber);
-        
+
         inJump = recentJump || (sameCollision && !groundCollision);
         grounded = groundCollision || recentColl;
-        
+
         if (grounded)
         {
             if (!inJump)
@@ -136,13 +153,33 @@ public class PlayerControls : MonoBehaviour, IPunObservable
                     lastJumpTime = currentTime;
                 }
             }
+
             moveDirectionState = kbMoveDirection;
             moveSpeedState = kbMoveSpeed;
         }
+
         if (kbMoveDirection != 0) lookDirectionState = kbMoveDirection;
-        
-        var speed = movementSpeed * ((moveSpeedState == 0)? 1.0f : movementAcceleration);
-        transform.Translate(moveDirectionState * speed * Time.deltaTime, 0, 0);
+
+        var speed = movementSpeed * ((moveSpeedState == 0) ? 1.0f : movementAcceleration);
+        rigidBody.velocity = new Vector2(moveDirectionState * speed, rigidBody.velocity.y);
+    }
+
+    private void FireBalls()
+    {
+        if (isFiringBalls)
+        {
+            var cantFire = IsWithin(PhotonNetwork.Time - lastFireballTime, 0.0, fireballInterval);
+            if (cantFire) return;
+            lastFireballTime = PhotonNetwork.Time;
+            var currentPosition = transform.position;
+            var startPosition = new Vector3(currentPosition.x, currentPosition.y, currentPosition.z + 1.0f);
+            var fireballDirection = new Vector3(
+                fireballPoint.x - currentPosition.x, 
+                fireballPoint.y - currentPosition.y, 0.0f);
+            var rotation = Quaternion.FromToRotation(Vector3.right, fireballDirection);
+            var o = PhotonNetwork.Instantiate(fireballPrefab.name, startPosition, rotation);
+            o.GetComponent<FireballBehavior>().parent = gameObject;
+        }
     }
 
     void GetInputs()
@@ -150,39 +187,56 @@ public class PlayerControls : MonoBehaviour, IPunObservable
         kbMoveDirection = 0;
         kbJump = 0;
         kbMoveSpeed = 0;
-        if (Input.GetKey(KeyCode.LeftArrow)) kbMoveDirection -= 1;
-        if (Input.GetKey(KeyCode.RightArrow)) kbMoveDirection += 1;
+
+        var hor = Input.GetAxis("Horizontal");
+        if (hor > 0.2f) kbMoveDirection = 1;
+        else if (hor < -0.2f) kbMoveDirection = -1;
 
         if (joystick.Horizontal > 0.2f) kbMoveDirection = 1;
         else if (joystick.Horizontal < -0.2f) kbMoveDirection = -1;
         if (Math.Abs(joystick.Horizontal) > 0.7f) kbMoveSpeed = 1;
 
         if (joystick.Vertical > 0.3f) kbJump = 1;
-        
+
         if (Input.GetKey(KeyCode.Space)) kbJump = 1;
         if (Input.GetKey(KeyCode.LeftShift)) kbMoveSpeed = 1;
+        
+        
+        isFiringBalls = false;
+        if (Input.GetMouseButton(0))
+        {
+            fireballPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            isFiringBalls = true;
+        }
+        // else if (Input.touchCount > 0)
+        // {
+        //     var ts = Input.touches.Where(t => eventSystem.IsPointerOverGameObject(t.fingerId));
+        //     if (ts.Any())
+        //     {
+        //         fireballPoint = Camera.main.ScreenToWorldPoint(ts.First().position);
+        //         isFiringBalls = true;
+        //     }
+        // }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (photonView.IsMine) {
-            if (other.gameObject.name == "Tilemap-base")
-            {
-                groundCollision = true;
-                groundCollisionNumber += 1;
-                groundCollisionEnter = PhotonNetwork.Time;
-                Debug.Log("Item grounded " + groundCollisionNumber + " times");
-            }
-            else
-            {
-                Debug.Log("Item touched game object '" + other.gameObject.name + "'");
-            }
-        }
+        if (!HasControl()) return;
+        // if (other.gameObject.name != "Tilemap-base") return;
+        groundCollision = true;
+        groundCollisionNumber += 1;
+        groundCollisionEnter = PhotonNetwork.Time;
+        Debug.Log("Item grounded " + groundCollisionNumber + " times");
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (!groundCollision) OnCollisionEnter2D(other);
     }
 
     private void OnCollisionExit2D(Collision2D other)
     {
-        if (photonView.IsMine)
+        if (HasControl())
         {
             groundCollision = false;
             groundCollisionExit = PhotonNetwork.Time;
@@ -198,6 +252,7 @@ public class PlayerControls : MonoBehaviour, IPunObservable
             {
                 lookDirectionCast = (byte) lookDirectionState;
             }
+
             stream.SendNext(lookDirectionCast);
             stream.SendNext(animationState);
         }
@@ -208,6 +263,7 @@ public class PlayerControls : MonoBehaviour, IPunObservable
             {
                 lookDirectionState = (sbyte) lookDirectionCast;
             }
+
             animationState = (AnimationState) stream.ReceiveNext();
         }
     }
@@ -222,12 +278,15 @@ public class PlayerControls : MonoBehaviour, IPunObservable
    Last Jump Collision Number: {lastJumpGroundCollisionNumber}
                    Is in jump: {inJump}
                   Is grounded: {grounded}
+              Is firing balls: {isFiringBalls}
 ");
     }
+
     private string MovemenDirectionToSymbol()
     {
         return (moveDirectionState == 0) ? "_" : (moveDirectionState < 0) ? "<" : ">";
     }
+
     private bool IsWithin(double x, double left, double right)
     {
         return (left < x) && (x < right);
